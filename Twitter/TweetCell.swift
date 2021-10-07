c7a18ecd1846a4bc1129d85ec6fc0aa4cf49ed08
FanXia0926 <78517859+FanXia0926@users.noreply.github.com>
2021-10-06 23:46:44 -0700

    Twitter finished

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,13 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Fan Xia on 10/5/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCellController {
+
+}
